758c8b97620a52161234d2e9c552fc755a94965e
Teemukissa <tremppu@hotmail.com>
2024-11-10 22:21:18 +0200

    refactor: move test content to index page

--- apps/react-testing/main.tsx
+++ apps/react-testing/main.tsx
@@ -2,52 +2,13 @@
 
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import {
-  HeadingBase,
-  LargeHeading,
-  LargeHeadingH1,
-} from './components/heading/heading.salty';
-import { Wrapper } from './components/wrapper/wrapper.styled';
-import {
-  Button,
-  ButtonsWrapper,
-  LargeButton,
-} from './components/button/button.css';
+import { IndexPage } from './pages/index-page';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <Wrapper className="theme-light">
-      <LargeHeadingH1>Large heading H1</LargeHeadingH1>
-      <LargeHeading>Large heading H2</LargeHeading>
-      <HeadingBase>Heading base H3</HeadingBase>
-
-      <ButtonsWrapper>
-        <Button
-          variant="solid"
-          borderRadius="regular"
-          onClick={() => alert('It is a button.')}
-          href="https://google.com"
-        >
-          Solid
-        </Button>
-        <Button variant="outlined" onClick={() => alert('It is a button.')}>
-          Outlined
-        </Button>
-      </ButtonsWrapper>
-
-      <ButtonsWrapper>
-        <LargeButton
-          warning
-          disabled
-          variant="solid"
-          onClick={() => alert('It is a button.')}
-        >
-          Large and red
-        </LargeButton>
-      </ButtonsWrapper>
-    </Wrapper>
+    <IndexPage />
   </StrictMode>
 );
---
+++ apps/react-testing/pages/index-page.tsx
@@ -0,0 +1,50 @@
+import { useRef } from 'react';
+import {
+  ButtonsWrapper,
+  Button,
+  LargeButton,
+} from '../components/button/button.css';
+import {
+  LargeHeadingH1,
+  LargeHeading,
+  HeadingBase,
+} from '../components/heading/heading.salty';
+import { Wrapper } from '../components/wrapper/wrapper.styled';
+
+export const IndexPage = () => {
+  const wrapper = useRef<HTMLElement>(null);
+
+  return (
+    <Wrapper className="theme-light" ref={wrapper}>
+      <LargeHeadingH1 id="main-heading">Large heading H1</LargeHeadingH1>
+      <LargeHeading>Large heading H2</LargeHeading>
+      <HeadingBase>Heading base H3</HeadingBase>
+
+      <ButtonsWrapper>
+        <Button
+          variant="solid"
+          borderRadius="regular"
+          onClick={() => alert('It is a button.')}
+          href="https://google.com"
+        >
+          Solid
+        </Button>
+        <Button variant="outlined" onClick={() => alert('It is a button.')}>
+          Outlined
+        </Button>
+      </ButtonsWrapper>
+
+      <ButtonsWrapper>
+        <LargeButton
+          warning
+          disabled
+          variant="solid"
+          onClick={() => alert('It is a button.')}
+          asd="123"
+        >
+          Large and red
+        </LargeButton>
+      </ButtonsWrapper>
+    </Wrapper>
+  );
+};
