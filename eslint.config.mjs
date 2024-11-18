c64c4e6f7407ff93ad38de097cc3fd8edd2256c8
Teemukissa <tremppu@hotmail.com>
2024-11-18 20:15:05 +0200

    chore: use flat config files for eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import nxEslintPlugin from '@nx/eslint-plugin';
+const { configs } = nxEslintPlugin;
+
+export default [
+  ...configs['flat/base'],
+  ...configs['flat/typescript'],
+  ...configs['flat/javascript'],
+  {
+    ignores: ['**/dist'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    // Override or add rules here
+    rules: {},
+  },
+];
