1539ec0344c44bce7013fc2a686c216517691d7a
Teemukissa <tremppu@hotmail.com>
2024-11-24 14:28:29 +0200

    fix(core): packageNames missing space

--- libs/core/src/bin/bin-util.ts
+++ libs/core/src/bin/bin-util.ts
@@ -11,9 +11,9 @@ export const execAsync = (command: string) => {
 };
 
 export const npmInstall = async (...packages: string[]) => {
-  const packageNames = packages.map((p) => p.replace('-D', '').split('@').slice(0, -1).join('@').trim());
-  const spinner = ora(`Installing packages: ${packageNames.join(', ')}`).start();
+  const packageNames = packages.map((p) => p.replace('-D', '').split('@').slice(0, -1).join('@').trim()).join(', ');
+  const spinner = ora(`Installing packages: ${packageNames}`).start();
   const asString = packages.join(' ');
   await execAsync(`npm install ${asString}`);
-  spinner.succeed(`Installed packages: ${packageNames.join(',')}`);
+  spinner.succeed(`Installed packages: ${packageNames}`);
 };
