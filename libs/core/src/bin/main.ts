06f528ca78a90e728c7246b87644fe2c0cdf6206
Teemukissa <tremppu@hotmail.com>
2024-11-25 22:50:48 +0200

    feat(core): add version command

--- libs/core/src/bin/main.ts
+++ libs/core/src/bin/main.ts
@@ -347,16 +347,18 @@ export async function main() {
     });
 
   interface UpdateOptions {
+    version?: string;
     legacyPeerDeps: boolean;
   }
 
   program
-    .command('update')
+    .command('update [version]')
     .alias('up')
-    .description('Update Salty-CSS packages to the latest version.')
+    .description('Update Salty-CSS packages to the latest or specified version.')
+    .option('-v, --version <version>', 'Version to update to.')
     .option('--legacy-peer-deps <legacyPeerDeps>', 'Use legacy peer dependencies (not recommended).', false)
-    .action(async function (this: Command) {
-      const { legacyPeerDeps } = this.opts<UpdateOptions>();
+    .action(async function (this: Command, _version = 'latest') {
+      const { legacyPeerDeps, version = _version } = this.opts<UpdateOptions>();
       const packageJSONPath = join(process.cwd(), 'package.json');
       const packageJson = await readPackageJson(packageJSONPath).catch((err) => logError(err));
       if (!packageJson) return;
@@ -369,7 +371,8 @@ export async function main() {
         );
       }
       const packagesToUpdate = saltyCssPackages.map((dep) => {
-        return `${dep}@${currentPackageJson.version}`;
+        if (version === '@') return `${dep}@${currentPackageJson.version}`;
+        return `${dep}@${version.replace(/^@/, '')}`;
       });
 
       if (legacyPeerDeps) {
@@ -380,6 +383,30 @@ export async function main() {
       }
 
       logger.info('Salty-CSS packages updated successfully!');
+    });
+
+  program
+    .command('version')
+    .alias('v')
+    .description('Show the current version of Salty-CSS.')
+    .action(async function () {
+      const currentPackageJson = await readThisPackageJson();
+      logger.info('CLI is running:', currentPackageJson.version);
+
+      const packageJSONPath = join(process.cwd(), 'package.json');
+      const packageJson = await readPackageJson(packageJSONPath).catch((err) => logError(err));
+      if (!packageJson) return;
+      const allDependencies = { ...packageJson.dependencies, ...packageJson.devDependencies };
+      const saltyCssPackages = Object.keys(allDependencies).filter((dep) => dep === 'salty-css' || dep.startsWith('@salty-css/'));
+      if (!saltyCssPackages.length) {
+        return logError(
+          'No Salty-CSS packages found in package.json. Make sure you are running update command in the same directory! Used package.json path: ' +
+            packageJSONPath
+        );
+      }
+      for (const dep of saltyCssPackages) {
+        logger.info(dep, allDependencies[dep]);
+      }
     });
 
   program.parseAsync(process.argv);
