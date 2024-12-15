b30cde61d0236fe9a1c08e4bd032521aabe4c2dd
Teemukissa <tremppu@hotmail.com>
2024-12-15 23:21:12 +0200

    chore: add npm-create package to support `npm create salty-css`

--- scripts/update-readmes.mjs
+++ scripts/update-readmes.mjs
@@ -1,13 +1,9 @@
 import { getFile, setFile } from './script-helpers.mjs';
 
-const packages = ['core', 'react', 'next', 'cli', 'eslint-config-core', 'eslint-plugin-core', 'vite', 'webpack'];
+const packages = ['core', 'react', 'next', 'cli', 'npm-create', 'eslint-config-core', 'eslint-plugin-core', 'vite', 'webpack'];
 
 const rootPath = '../README.md';
-const corePath = '../dist/libs/margarita-form/README.md';
-const reactPath = '../dist/libs/margarita-form-react/README.md';
-const angularPath = '../dist/libs/margarita-form-angular/README.md';
 const readmeContents = await getFile(rootPath, false);
-const libsToUpdate = [corePath, reactPath, angularPath];
 
 await Promise.all(
   packages.map(async (dist) => {
