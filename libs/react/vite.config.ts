b75c78e11014ce3df6dcc784251d3201b56b993d
Teemukissa <tremppu@hotmail.com>
2024-12-19 09:00:05 +0200

    chore(react): add react-dom and react/jsx-runtime as external

--- libs/react/vite.config.ts
+++ libs/react/vite.config.ts
@@ -52,7 +52,7 @@ export default defineConfig({
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
-      external: ['@salty-css/core', 'path', 'fs', 'fs/promises', 'esbuild', 'winston', 'child_process', 'react'],
+      external: ['@salty-css/core', 'path', 'fs', 'fs/promises', 'esbuild', 'winston', 'child_process', 'react', 'react-dom', 'react/jsx-runtime'],
     },
   },
   test: {
