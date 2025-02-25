0507fab69596717e25b94cec960105f9bf734256
Teemukissa <tremppu@hotmail.com>
2025-02-25 08:54:10 +0200

    fix(core): add check to ensure that templates are defined in getTemplateTypes

--- libs/core/src/generator/parse-templates.ts
+++ libs/core/src/generator/parse-templates.ts
@@ -32,6 +32,7 @@ export const getTemplateKeys = <T extends object>(templates: T): string[] => {
 };
 
 export const getTemplateTypes = <T extends object>(templates: T): Record<string, string> => {
+  if (!templates) return {};
   return Object.entries(templates).reduce((acc, [key, value]) => {
     if (typeof value === 'object')
       acc[key] = getTemplateTokens(value)
