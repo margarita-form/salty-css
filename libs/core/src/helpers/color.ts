6c24a3c70d08cc8fe026ef3fc35dce529bbf7d90
Teemukissa <tremppu@hotmail.com>
2025-03-07 09:10:33 +0200

    fix(core): color helper breaks when multiple methods used

--- libs/core/src/helpers/color.ts
+++ libs/core/src/helpers/color.ts
@@ -1,13 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import colorFunction, { ColorInstance, ColorLike } from 'color';
 
+type BaseColor = Omit<ColorLike, string> | PropertyValueToken;
+
 class Color {
   public isColor = true;
   public currentColor: ColorInstance;
 
-  constructor(base: ColorLike) {
-    this.currentColor = colorFunction(base);
+  constructor(public base: BaseColor) {
+    const colorValue = this._resolveBaseColor(base);
+    this.currentColor = colorFunction(colorValue);
+    return this._createProxy();
+  }
 
+  private _createProxy() {
     return new Proxy(this, {
       get(target: any, prop: any) {
         if (prop in target) return target[prop];
@@ -17,12 +23,19 @@ class Color {
     });
   }
 
+  private _resolveBaseColor(base: BaseColor) {
+    if (typeof base !== 'string') return base;
+    const isToken = /\{[^{}]+\}/g.test(base);
+    if (!isToken) return base;
+    throw new Error('Token values are not yet supported, use a color string instead');
+  }
+
   public _handleColorMethod(method: keyof ColorInstance) {
     const color = this.currentColor as Record<keyof ColorInstance, any>;
     if (typeof color[method] !== 'function') return color[method];
     return (...args: any[]) => {
       this.currentColor = color[method](...args);
-      return this;
+      return this._createProxy();
     };
   }
 
@@ -31,6 +44,6 @@ class Color {
   }
 }
 
-export const color = (value: ColorLike) => {
+export const color = (value: BaseColor) => {
   return new Color(value) as Color & ColorInstance;
 };
