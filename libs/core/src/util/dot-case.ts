a59f3727307dafa5f4f88e856b007b20a7e084b2
Teemukissa <tremppu@hotmail.com>
2025-04-04 00:20:36 +0300

    fix(core): refactor variable naming to keep camelCase names

--- libs/core/src/util/dot-case.ts
+++ libs/core/src/util/dot-case.ts
@@ -1,7 +1,5 @@
 export function dotCase(str: PropertyKey): string {
   if (!str) return '';
   if (typeof str !== 'string') return dotCase(String(str));
-  return str.replace(/[\s-]/g, '.').replace(/[A-Z](?:(?=[^A-Z])|[A-Z]*(?=[A-Z][^A-Z]|$))/g, (s, i) => {
-    return (i > 0 ? '.' : '') + s.toLowerCase();
-  });
+  return str.replace(/[^\d\w]/g, '.');
 }
