ee1f7600b18a2978dcbc021789a741038fc2f651
Teemukissa <tremppu@hotmail.com>
2026-01-20 21:23:35 +0200

    chore: add log

--- libs/core/src/cache/resolve-dynamic-config-cache.ts
+++ libs/core/src/cache/resolve-dynamic-config-cache.ts
@@ -4,11 +4,12 @@ import { join } from 'path';
 export const resolveDynamicConfigCache = async () => {
   const currentDir = process.cwd();
   const filename = 'config-cache.json';
-  const patterns = ['', 'saltygen', 'src', 'src/saltygen', 'cache', 'src/cache', 'src/saltygen/cache'];
+  const patterns = ['', 'saltygen', 'src', 'src/saltygen', 'cache', 'src/cache', 'saltygen/cache', 'src/saltygen/cache'];
 
   let contents = '';
   for (const pattern of patterns) {
     const potentialPath = join(currentDir, pattern, filename);
+    console.log(`Checking for config cache at: ${potentialPath}`);
     try {
       contents = await readFile(potentialPath, 'utf8');
       break;
