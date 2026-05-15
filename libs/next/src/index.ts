bee8fadabb5148c1de606e9f24ef9d7f45693abe
Teemukissa <tremppu@hotmail.com>
2026-05-15 10:24:36 +0300

    fix(next): arg types for withSaltyCss were too strict

--- libs/next/src/index.ts
+++ libs/next/src/index.ts
@@ -1,13 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { saltyPlugin } from '@salty-css/webpack';
 import type { SaltyCompilerMode } from '@salty-css/core/compiler/salty-compiler';
 
-type NextWebpackFNLike = (config: any, options: any) => any;
-
 type AnyRecord = Record<string, unknown>;
-
-type NextConfigLikeResult = {
-  webpack?: null | undefined | NextWebpackFNLike;
-};
 
 export interface SaltyNextOptions {
   /**
@@ -16,22 +11,19 @@ export interface SaltyNextOptions {
   mode?: SaltyCompilerMode;
 }
 
-export const withSaltyCss = <T extends AnyRecord & NextConfigLikeResult>(
-  nextConfig: T,
-  saltyOptions: SaltyNextOptions = {}
-): T & NextConfigLikeResult => {
+export const withSaltyCss = <T extends AnyRecord>(nextConfig: T, saltyOptions: SaltyNextOptions = {}) => {
   const { webpack: incomingWebpack, ...rest } = nextConfig;
   return {
     ...rest,
-    webpack(config, options) {
+    webpack(config: any, options: any) {
       const { dir, isServer } = options;
       saltyPlugin(config, dir, isServer, false, { mode: saltyOptions.mode });
-      if (incomingWebpack) {
+      if (incomingWebpack && typeof incomingWebpack === 'function') {
         incomingWebpack(config, options);
       }
       return config;
     },
-  } as T & NextConfigLikeResult;
+  };
 };
 
 export default withSaltyCss;
